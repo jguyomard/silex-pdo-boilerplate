4eba819432c5ddafb007cf61a14bfc11c4cf79e4
Julien <julien@mangue.eu>
2016-04-16 15:49:35 +0200

    PHP built-in server: handle static content

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// PHP built-in server: let the server handle the request as-is for static files
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
